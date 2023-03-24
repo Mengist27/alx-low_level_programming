a469d5464c130eb9f6d9788f4297dcacaffa3b0c
Mengist27 <setew27@gmail.com>
2023-03-25 02:27:29 +0300

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,7 +17,7 @@ int main(void)
 	{
 		if (i == 100)
 			printf("%s ", b);
-		else if (i % 3 == 0 && i % 5 == 0)
+		else if ((i % 3 == 0) && (i % 5 == 0))
 			printf("%s ", ab);
 		else if (i % 3 == 0)
 			printf("%s ", a);
