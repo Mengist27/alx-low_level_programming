6b98deb45f86c29e2f30bccecdde41d6038c8e5d
Mengist27 <setew27@gmail.com>
2023-03-30 16:54:02 +0300

    TAsk Five

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,6 +10,8 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+char *string_toupper(char *str);
+char *string_toupper(char *str);
 
 
 #endif
