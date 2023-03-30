d88bac26a212292189949f6511a717f82235dbb4
Mengist27 <setew27@gmail.com>
2023-03-30 13:22:54 +0300

    Task Four

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,15 +2,17 @@
 
 /**
  * _strcmp - compare string values
- * @s1: input value
- * @s2: inpute value
+ * @s1: strings
+ * @s2: strings
  * Return: 0
  */
+
 int _strcmp(char *s1, char *s2)
 {
 	int i;
 
 	i = 0;
+
 	for (i = 0 ; s1[i] != '\0' || s2[i] != '\0' ; i++)
 	{
 		if (s1[i] != s2[i])
@@ -21,7 +23,7 @@ int _strcmp(char *s1, char *s2)
 				return (s1[i] - s2[i]);
 		}
 		else
-		return (0);
+			return (0);
 	}
 	return (0);
 }
